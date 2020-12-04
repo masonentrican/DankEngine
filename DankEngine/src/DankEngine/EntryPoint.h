fa2fd233b79586cffefd35c2750d958133655084
Mason Entrican <mason@entrican.com>
2020-12-03 20:11:55 -0800

    Added spdlog as a submodule. Implemented a log h and source. Dank.h includes Log.h. Macros created in Log.h for easy logging in both engine and client. TODO: Entrypoint currently inits log and adds test warnings. This should be done in engine not client

--- DankEngine/src/DankEngine/EntryPoint.h
+++ DankEngine/src/DankEngine/EntryPoint.h
@@ -6,6 +6,10 @@ extern Dank::Application* Dank::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Dank::Log::Init();
+	DANK_CORE_WARN("Initialized Log");
+	DANK_INFO("App has started");
+
 	auto app = Dank::CreateApplication();
 	app->Run();
 	delete app;
---
+++ DankEngine/src/DankEngine/Log.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace Dank {
+
+	class DANK_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+
+}
+
+#define DANK_CORE_TRACE(...)   ::Dank::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define DANK_CORE_INFO(...)    ::Dank::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define DANK_CORE_WARN(...)    ::Dank::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define DANK_CORE_ERROR(...)   ::Dank::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define DANK_CORE_FATAL(...)   ::Dank::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+#define DANK_TRACE(...)   ::Dank::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define DANK_INFO(...)    ::Dank::Log::GetClientLogger()->info(__VA_ARGS__)
+#define DANK_WARN(...)    ::Dank::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define DANK_ERROR(...)   ::Dank::Log::GetClientLogger()->error(__VA_ARGS__)
+#define DANK_FATAL(...)   ::Dank::Log::GetClientLogger()->fatal(__VA_ARGS__)

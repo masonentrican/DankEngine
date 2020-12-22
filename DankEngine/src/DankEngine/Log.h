71a4fe2b058a0ad2b7feca023ef16095cb264434
Mason Entrican <mason@entrican.com>
2020-12-21 18:47:45 -0800

    Removed sln, proj files, and reset ini

--- DankEngine/src/DankEngine/Log.h
+++ DankEngine/src/DankEngine/Log.h
@@ -29,10 +29,10 @@ namespace Dank {
 #define DANK_CORE_INFO(...)    ::Dank::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define DANK_CORE_WARN(...)    ::Dank::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define DANK_CORE_ERROR(...)   ::Dank::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define DANK_CORE_FATAL(...)   ::Dank::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define DANK_CORE_FATAL(...)   ::Dank::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 #define DANK_TRACE(...)   ::Dank::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define DANK_INFO(...)    ::Dank::Log::GetClientLogger()->info(__VA_ARGS__)
 #define DANK_WARN(...)    ::Dank::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define DANK_ERROR(...)   ::Dank::Log::GetClientLogger()->error(__VA_ARGS__)
-#define DANK_FATAL(...)   ::Dank::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define DANK_FATAL(...)   ::Dank::Log::GetClientLogger()->critical(__VA_ARGS__)

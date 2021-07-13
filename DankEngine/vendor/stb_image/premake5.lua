44e7b94f586b78a752b2773595088b6424a0d2ae
gilsdank <Gilmarquez77@gmail.com>
2021-07-13 17:41:21 -0600

    initial textures push

---
+++ DankEngine/vendor/stb_image/premake5.lua
@@ -0,0 +1,24 @@
+project "stb_image"
+	kind "StaticLib"
+	language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"stb_image.h"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"

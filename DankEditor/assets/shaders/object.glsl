a16acbf6f56126567faaaed9f2f9abb89f98cd0b
gilsdank <Gilmarquez77@gmail.com>
2021-08-27 13:59:31 -0600

    fixed floating point error in shader. Probably a better way to do it but i havent been able to find a better solution.

--- DankEditor/assets/shaders/object.glsl
+++ DankEditor/assets/shaders/object.glsl
@@ -75,25 +75,25 @@ uniform vec3 lightColor;
 uniform vec3 lightPos;
 uniform vec3 viewPos;
 
+
 void main()
 {    
     if(int(TexturesLoaded) == 1) {
-    
     //ambient
     //vec3 ambient = light.ambient * vec3(texture(texture_diffuse1, TexCoords));
-    vec3 ambient = light.ambient * vec3(texture(u_Textures[int(v_TexIndex - 1)], TexCoords));
+    vec3 ambient = light.ambient * vec3(texture(u_Textures[int(v_TexIndex + 0.1) - 1], TexCoords));
     //diffuse
     vec3 norm = normalize(Normal);
     
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * diff * vec3(texture(u_Textures[int(v_TexIndex - 1)], TexCoords));
+    vec3 diffuse = light.diffuse * diff * vec3(texture(u_Textures[int(v_TexIndex + 0.1) - 1], TexCoords));
 
        //specular
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = light.specular * spec * texture(u_Textures[int(v_TexIndex)], TexCoords).rgb;
+    vec3 specular = light.specular * spec * texture(u_Textures[int(v_TexIndex + 0.1)], TexCoords).rgb;
     //vec3 specular = light.specular * (spec * material.specular); 
 
     vec3 result = ambient + diffuse + specular; 
